75803b09bd5ee92f472f40df7780338cea980dd1
lenaglisic25 <166614046+lenaglisic25@users.noreply.github.com>
2024-04-10 08:43:36 -0400

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -8,15 +8,15 @@ def encode(number):
         encoded_number += encoded_digit
     return int(encoded_number)
 
-# def decode(encoded_number):
-#     encoded_str = str(encoded_number)
-#     decoded_number = ''
-#     for char in encoded_str:
-#         # Convert the character to an integer, subtract 3 from it, and convert it back to a string
-#         decoded_digit = str(int(char) - 3)
-#         # Append the decoded digit to the decoded number string
-#         decoded_number += decoded_digit
-#     return int(decoded_number)
+def decode(encoded_number):
+    encoded_str = str(encoded_number)
+    decoded_number = ''
+    for char in encoded_str:
+        # Convert the character to an integer, subtract 3 from it, and convert it back to a string
+        decoded_digit = str(int(char) - 3)
+        # Append the decoded digit to the decoded number string
+        decoded_number += decoded_digit
+    return int(decoded_number)
 
 def main():
     while True:
@@ -32,9 +32,9 @@ def main():
             number = int(input("Please enter your password to encode: "))
             encoded_number = encode(number)
             print("Your password has been encoded and stored!")
-        # elif choice == "2":
-        #     decoded_number = decode(encoded_number)
-        #     print(f"The encoded password is {decoded_number}, and the original password is {encoded_number}.")
+        elif choice == "2":
+            decoded_number = decode(encoded_number)
+            print(f"The encoded password is {decoded_number}, and the original password is {encoded_number}.")
         elif choice == "3":
             break
         else:
